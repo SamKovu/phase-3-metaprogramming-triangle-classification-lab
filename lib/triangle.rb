aefdeaf85d890b85894b9590f13053143ed55e8b
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-05-22 04:51:45 +0300

    Completed lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+ 
+   attr_accessor :side1, :side2 , :side3
+
+  def initialize(side_1,side_2,side_3)
+    @side1=side_1
+    @side2=side_2
+    @side3=side_3
+  end
+
+  def kind
+    
+    if (self.side1+self.side2>self.side3 && self.side1+self.side3>self.side2 && self.side2+self.side3>self.side1) && (self.side1>0 && self.side2>0 && self.side3>0)
+   
+        if (self.side1==self.side2 && self.side2==self.side3)
+          :equilateral 
+        elsif (self.side1!=self.side2 && self.side2!=self.side3 && self.side1!=self.side3) 
+          :scalene 
+        elsif (self.side1==self.side2 || self.side2==self.side3 || self.side1==self.side3)
+          :isosceles
+        end
+
+    else
+     
+      raise TriangleError
+        
+    end
+ 
+  end
+
+  
+   
+  class TriangleError < StandardError
+
+  end
+
+
 end
